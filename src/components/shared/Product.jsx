2f88cc8bdb11984a991a6c123cb0ba26254dcf28
badr elyatim <flipbadr7@gmail.com>
2024-08-12 20:49:12 +0100

    popular now, just dropped sections

--- src/components/shared/Product.jsx
+++ src/components/shared/Product.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { CiHeart } from "react-icons/ci";
 
-function Product() {
-  return (
-    <div>Product</div>
+function Product({color, img}) {
+    return (
+        <div>
+            <div className='relative flex justify-center items-center rounded-2xl h-[270px]' style={{backgroundColor: color}}>
+                <img src={img} alt="" />
+
+                <div className='rounded-full p-2 absolute right-3 top-3 bg-white cursor-pointer'>
+                    <CiHeart color='black' size={19} />
+                </div>
+            </div>
+            <h3 className='font-aBeeZee text-base mt-1' style={{color: color}}>Hental</h3>
+            <h4 className='font-alatsi text-xl mt-1'>Hental T-Shirt</h4>
+            <p className='font-alatsi text-[#666666] text-sm'>€30,95</p>  
+        </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import PopularNow from '../components/PopularNow'
+import JustDropped from '../components/JustDropped'
 
 export default function store() {
   return (
     <>
       {/* <Navbar /> */}
       <PopularNow />
+      <JustDropped />
     </>
   )
 }
